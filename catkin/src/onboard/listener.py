4b35ec08e465e9a5df5f45f4792bcf290c19a3b3
Matt Vollrath <matt@endpoint.com>
2014-06-25 13:25:42 -0700

    Onboard keyboard ROS listener

---
+++ catkin/src/onboard/listener.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# This app sends messages to dbus signalling visibility of the onboard on-screen keyboard.
+
+import os
+import rospy
+from std_msgs.msg import Bool
+
+def callback(data):
+  # TODO: use python dbus bindings instead of dbus cli
+  if data.data:
+    os.system('dbus-send --type=method_call --dest=org.onboard.Onboard /org/onboard/Onboard/Keyboard org.onboard.Onboard.Keyboard.Show')
+  else:
+    os.system('dbus-send --type=method_call --dest=org.onboard.Onboard /org/onboard/Onboard/Keyboard org.onboard.Onboard.Keyboard.Hide')
+
+def listener():
+  rospy.init_node('onboard_listener')
+  rospy.Subscriber('onboard/visibility', Bool, callback)
+
+  rospy.spin()
+
+if __name__=='__main__':
+  listener()
