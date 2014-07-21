bdc582f3251f3db3c44742688f7958ddca74347d
wojtek <wojtek@endpoint.com>
2014-07-21 14:59:35 +0200

    Rewrote threejs part

--- extensions/feedback_arrows/apply.js
+++ extensions/feedback_arrows/apply.js
@@ -52,6 +52,152 @@ feedbackArrowsSpacenavListener.subscribe(function(msg){
 
 
 // Three.js part
+var container, stats;
+
+var camera, scene, renderer;
+
+var mouseX = 0, mouseY = 0;
+
+var windowHalfX = window.innerWidth / 2;
+var windowHalfY = window.innerHeight / 2;
+
+
+console.log("Arrows: before init");
+init();
+console.log("Arrows: after init");
+console.log("Arrows: before animate");
+animate();
+console.log("Arrows: after animate");
+
+function init() {
+
+	container = document.createElement( 'div' );
+	container.id = 'feedback_arrows';
+	document.body.appendChild( container );
+
+	camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
+	camera.position.z = 100;
+
+	// scene
+
+	scene = new THREE.Scene();
+
+	var ambient = new THREE.AmbientLight( 0x101030 );
+	scene.add( ambient );
+
+	var directionalLight = new THREE.DirectionalLight( 0xffeedd );
+	directionalLight.position.set( 0, 0, 1 );
+	scene.add( directionalLight );
+
+	// texture
+
+	var manager = new THREE.LoadingManager();
+	manager.onProgress = function ( item, loaded, total ) {
+
+		console.log( item, loaded, total );
+
+	};
+
+	var texture = new THREE.Texture();
+
+	var loader = new THREE.ImageLoader( manager );
+	loader.load( 'arrows_texture.gif', function ( image ) {
+
+		texture.image = image;
+		texture.needsUpdate = true;
+
+	} );
+
+	// model
+
+	var loader = new THREE.OBJLoader( manager );
+	loader.load( 'arrows.obj', function ( object ) {
+
+		object.traverse( function ( child ) {
+
+			if ( child instanceof THREE.Mesh ) {
+
+				child.material.map = texture;
+
+			}
+
+		} );
+
+		object.position.y = - 80;
+		scene.add( object );
+
+	} );
+
+	//
+
+	renderer = new THREE.WebGLRenderer();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	container.appendChild( renderer.domElement );
+
+	document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+
+	//
+
+	window.addEventListener( 'resize', onWindowResize, false );
+
+}
+
+function onWindowResize() {
+
+	windowHalfX = window.innerWidth / 2;
+	windowHalfY = window.innerHeight / 2;
+
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+
+	renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+function onDocumentMouseMove( event ) {
+
+	mouseX = ( event.clientX - windowHalfX ) / 2;
+	mouseY = ( event.clientY - windowHalfY ) / 2;
+
+}
+
+//
+
+function animate() {
+
+	requestAnimationFrame( animate );
+	render();
+
+}
+
+function render() {
+
+	camera.position.x += ( mouseX - camera.position.x ) * .05;
+	camera.position.y += ( - mouseY - camera.position.y ) * .05;
+
+	camera.lookAt( scene.position );
+
+	renderer.render( scene, camera );
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/***
+
+
+
+
 var container;
 var camera, scene, renderer;
 var mouseX = 0, mouseY = 0;
@@ -60,7 +206,7 @@ var windowHalfX = window.innerWidth / 2;
 var windowHalfY = window.innerHeight / 2;
 
 
-/*** Initialize ***/
+
 function init() {
   // This <div> will host the canvas for our scene.
   container = document.createElement( 'div' );
@@ -89,7 +235,7 @@ function init() {
   directionalLight.position.set( 0, 0, 1 ).normalize();
   scene.add( directionalLight );
 
-  /*** Texture Loading ***/
+ 
   var manager = new THREE.LoadingManager();
   manager.onProgress = function ( item, loaded, total ) {
     console.log( "Feedback arrows threejs manager => ", item, loaded, total );
@@ -150,7 +296,6 @@ function init() {
   console.log('Loading Feedback arrows Extension: FINISHED');
 }
 
-/*** The Loop ***/
 function animate() {
   // This function calls itself on every frame. You can for example change
   // the objects rotation on every call to create a turntable animation.
@@ -166,3 +311,4 @@ console.log('Loading Feedback arrows Extension: Initializing three.js');
 init();
 console.log('Loading Feedback arrows Extension: Starting animation');
 animate();
+***/
