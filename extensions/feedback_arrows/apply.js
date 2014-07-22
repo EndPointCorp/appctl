ab2685a2f96998a56370cdad743afa0d28faa907
wojtek <wojtek@endpoint.com>
2014-07-22 15:40:40 +0200

    Changed rotation factor

--- extensions/feedback_arrows/apply.js
+++ extensions/feedback_arrows/apply.js
@@ -110,9 +110,8 @@ feedbackArrowsSpacenavListener.subscribe(function(msg) {
 		// lean forward and backward
 		ringObjPosition[5] = this.msg.angular.x * -0.1;
 		ringObjPosition[3] = this.msg.angular.y * -0.1;
-		direction = Math.atan2(this.msg.linear.y/100,  this.msg.linear.x/100) / Math.PI * 180;
-		console.log("This is direction1:", direction/1000);
-		
+		direction = Math.atan2(this.msg.linear.y,  this.msg.linear.x) / Math.PI * 180;
+		console.log("This is direction1:", direction);
 		arrowObjPosition[4] = direction;
 		//arrowObjPosition[4] = 1; 
 	}
