7800481ddbbf24530e365b0ffe219edf122bc852
Szymon Guz <szymon@endpoint.com>
2014-08-16 15:23:16 +0200

    Add js file for switching browser

---
+++ extensions/display/contentjs/display_switch.js
@@ -0,0 +1,15 @@
+
+var slinkyRosDisplay = new ROSLIB.Ros({
+  url: 'wss://42-b:9090'
+});
+
+var displaySwitchTopic = new ROSLIB.Topic({
+  ros: slinkyRosDisplay,
+  name: '/display/switch',
+  messageType: 'std_msgs/String',
+});
+
+displaySwitchTopic.subscribe(function(msg){
+  var url = msg.data;
+  $('body').fadeOut(2000, function() { document.location = url } );
+});
