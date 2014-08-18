67ab13ddff8a90bb156b2a910ac866e6335c9d8f
Szymon Guz <szymon@endpoint.com>
2014-08-18 18:02:39 +0200

    Add tap event too

--- extensions/google_properties_menu/contentjs/doodles.js
+++ extensions/google_properties_menu/contentjs/doodles.js
@@ -1,8 +1,19 @@
-$('#pacman').click(function(){
+var runPacman = function() {
+
     $(this).addClass("selected");
     $("#choose_game").hide();
     $("#pacman_selected").show();
 
     // and send the ROS message
-    sendSwitchROSMessage();
+    sendSwitchROSMessage($(this));
+}
+
+$('#pacman').click(function(){
+  runPacman();
 });
+
+document.body.addEventListener(
+  'touchstart', 
+  runPacman
+  true
+);
