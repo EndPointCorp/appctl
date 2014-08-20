148dccb227baf6c95406fcdd0f88a77e7284c10c
Matt Vollrath <matt@endpoint.com>
2014-08-20 17:04:32 -0400

    Add relaunch to rfreceiver node

--- catkin/src/rfreceiver/scripts/sender.py
+++ catkin/src/rfreceiver/scripts/sender.py
@@ -1,25 +1,42 @@
 #!/usr/bin/env python
 
+import os
 import rospy
 import serial
+import subprocess
 from std_msgs.msg import Byte
 
+DEVNULL = open(os.devnull, 'w')
+CLEAR_BUTTON = 2
+
 def main():
-  buttondown_pub = rospy.Publisher('/rfreceiver/buttondown', Byte, queue_size = 1)
+  buttondown_pub = rospy.Publisher(
+    '/rfreceiver/buttondown',
+    Byte,
+    queue_size = 1
+  )
   rospy.init_node('rfreceiver')
 
-  receiver = serial.Serial('/dev/ttyACM0', 9600)
+  receiver = serial.Serial('/dev/promicro16.0', 9600)
 
   buf = ''
 
   while not rospy.is_shutdown():
     try:
-      msg = int(receiver.readline(10).strip())
+      button = int(receiver.readline(10).strip())
     except serial.SerialException as e:
       print e
       break
 
-    buttondown_pub.publish(Byte(msg))
+    # TODO(mv): move this to a more general slinky system ros interface
+    if button == CLEAR_BUTTON:
+      subprocess.call(
+        ['/home/lg/bin/lg-run-bg', 'pkill chrome'],
+        stdout=DEVNULL,
+        stderr=DEVNULL
+      )
+
+    buttondown_pub.publish(Byte(button))
 
 if __name__ == '__main__':
   try:
