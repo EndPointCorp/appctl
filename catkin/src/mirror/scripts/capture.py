67fc9ed2d4a65c480ac0e0c45948e198b54cf207
Matt Vollrath <matt@endpoint.com>
2014-10-25 07:43:20 -0400

    Add catkin mirror package for desktop mirroring

---
+++ catkin/src/mirror/scripts/capture.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+import rospy
+
+import appctl_support
+from appctl.msg import Mode
+from appctl.srv import Query
+
+GSTREAMER_LAUNCHER = 'gst-launch'
+
+CAPTURE_ARGS = [
+  'ximagesrc', 'display-name={display}', 'show-pointer=false', 'use-damage=false',
+  '!',
+  'videoscale',
+  '!',
+  'video/x-raw-rgb,width=1920,height=1080,framerate=60/1',
+  '!',
+  'ffmpegcolorspace',
+  '!',
+  'x264enc', 'tune=zerolatency', 'speed-preset=medium', 'threads=4',
+    'sliced-threads=true', 'bitrate=6000', 'byte-stream=true',
+  '!',
+  'tcpserversink', 'port={server_port}'
+]
+
+def main():
+  rospy.init_node('mirror_capture', anonymous=True)
+
+  modes = rospy.get_param('~modes').split(',')
+  server_port = str(rospy.get_param('~port', 4953))
+  display = rospy.get_param('~display', ':0')
+
+  rospy.loginfo('running on modes {}'.format(modes))
+
+  cmd = [GSTREAMER_LAUNCHER]
+
+  args = map(lambda arg: arg.format(
+    server_port = server_port,
+    display = display
+  ), CAPTURE_ARGS)
+
+  cmd.extend(args)
+
+  mode_handler = appctl_support.ModeHandler(modes, cmd)
+
+  rospy.Subscriber('/appctl/mode', Mode, mode_handler.handle_mode_msg)
+
+  # TODO(mv): check /appctl/query
+
+  rospy.spin()
+
+  mode_handler.shutdown()
+
+if __name__=='__main__':
+  main()
---
+++ catkin/src/mirror/scripts/playback.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+import rospy
+
+import appctl_support
+from appctl.msg import Mode
+from appctl.srv import Query
+
+GSTREAMER_LAUNCHER = 'gst-launch'
+
+PLAYBACK_ARGS = [
+  'tcpclientsrc', 'host={playback_host}', 'port={playback_port}',
+  '!',
+  'capsfilter', 'caps=video/x-h264,width=1920, height=1080,framerate=60/1, pixel-aspect-ratio=1/1, stream-format=byte-stream, alignment=au, level=4, profile=main',
+  '!',
+  'ffdec_h264',
+  '!',
+  'xvimagesink', 'display={display}'
+]
+
+def main():
+  rospy.init_node('mirror_playback', anonymous=True)
+
+  modes = rospy.get_param('~modes').split(',')
+  server_host = rospy.get_param('~host')
+  server_port = str(rospy.get_param('~port', 4953))
+  display = rospy.get_param('~display', ':0')
+
+  rospy.loginfo('running on modes {}'.format(modes))
+
+  cmd = [GSTREAMER_LAUNCHER]
+  args = map(lambda arg: arg.format(
+    playback_host = server_host,
+    playback_port = server_port,
+    display = display
+  ), PLAYBACK_ARGS)
+
+  cmd.extend(args)
+
+  mode_handler = appctl_support.ModeHandler(modes, cmd)
+
+  rospy.Subscriber('/appctl/mode', Mode, mode_handler.handle_mode_msg)
+
+  # TODO(mv): check /appctl/query
+
+  rospy.spin()
+
+  mode_handler.shutdown()
+
+if __name__=='__main__':
+  main()
