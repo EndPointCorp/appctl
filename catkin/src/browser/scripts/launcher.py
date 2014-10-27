8fc832d115a8ddb49b1e020ae7df521fca7ecaa8
Matt Vollrath <matt@endpoint.com>
2014-10-27 16:36:16 -0400

    Add more lg-browser opts to browser launcher

--- catkin/src/browser/scripts/launcher.py
+++ catkin/src/browser/scripts/launcher.py
@@ -15,17 +15,14 @@ def main():
   window = rospy.get_param('~window', 'lgS0')
   app = rospy.get_param('~app', 'appctl')
   url = rospy.get_param('~url')
+
   disable_extensions = rospy.get_param('~disable_extensions', False)
+  disable_kiosk = rospy.get_param('~disable_kiosk', False)
 
-  if rospy.has_param('~debug_port'):
-    debug_port = str(rospy.get_param('~debug_port'))
-  else:
-    debug_port = None
-
-  if rospy.has_param('~browser_bin'):
-    browser_bin = rospy.get_param('~browser_bin')
-  else:
-    browser_bin = None
+  debug_port = rospy.get_param('~debug_port', None)
+  browser_bin = rospy.get_param('~browser_bin', None)
+  scale_factor = rospy.get_param('~scale_factor', None)
+  user_agent = rospy.get_param('~user_agent', None)
 
   rospy.loginfo('running on modes {}'.format(modes))
 
@@ -35,12 +32,20 @@ def main():
   cmd.extend(('-u', url))
   if disable_extensions:
     cmd.append('-D')
+  if disable_kiosk:
+    cmd.append('-K')
   if debug_port is not None:
     cmd.extend(('-p', debug_port))
   if browser_bin is not None:
     cmd.extend(('-b', browser_bin))
+  if scale_factor is not None:
+    cmd.extend(('-s', scale_factor))
+  if user_agent is not None:
+    cmd.extend(('-U', user_agent))
 
-  mode_handler = appctl_support.ModeHandler(modes, cmd)
+  sanitized_cmd = map(lambda arg: str(arg), cmd)
+
+  mode_handler = appctl_support.ModeHandler(modes, sanitized_cmd)
 
   rospy.Subscriber('/appctl/mode', Mode, mode_handler.handle_mode_msg)
 
