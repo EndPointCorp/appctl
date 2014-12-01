f56f9b3b8fe7f208e92b44e6b04af1c40d8cdd7c
Matt Vollrath <matt@endpoint.com>
2014-12-01 14:17:23 -0800

    Change "More fun" back to Arcade

--- extensions/google_properties_menu/contentjs/google_properties_menu.js
+++ extensions/google_properties_menu/contentjs/google_properties_menu.js
@@ -17,18 +17,18 @@ var data = [
   },
   /*
   {
-    name: 'doodles',
-    desc: 'Arcade',
-    icon: 'doodles.png',
-    action: 'showDoodlesPage'
-  },
-  */
-  {
     name: 'timelapse',
     desc: 'Timelapse',
     icon: 'timelapse.png',
     action: 'gotoTimelapse',
     url: timelapseURL
+  },
+  */
+  {
+    name: 'doodles',
+    desc: 'Arcade',
+    icon: 'doodles.png',
+    action: 'showDoodlesPage'
   }
 ];
 
