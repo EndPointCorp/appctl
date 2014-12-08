7b0353569267e2ed61a2e912ed0e7d8198085ab0
Szymon Guz <szymon@endpoint.com>
2014-12-08 08:23:25 -0500

    Add config support for the google properties menu

--- extensions/google_properties_menu/contentjs/google_properties_menu.js
+++ extensions/google_properties_menu/contentjs/google_properties_menu.js
@@ -1,123 +1,141 @@
+/*
+  The configuration is read from the file taken from the dataURL variable.
+
+  A couple of remarks:
+  1 - the elements in the list are shown in the order
+      they are defined in the config file
+
+  2 - if there are no (display|kiosk)_url variables,
+      then tapping the icon does nothing
+
+  The example config looks like this:
+
+{
+    "portal": {
+        "morefun_items": [
+            {
+                "display_url": "http://lg-head/portal-loader.html",
+                "icon":        "https://lg-head/portal/morefun/icon_earth.png",
+                "kiosk_url":   "http://lg-head/portal-loader.html",
+                "name":        "Maps"
+            },
+            {
+                "display_url": "",
+                "icon":        "https://lg-head/portal/morefun/doodles.png",
+                "kiosk_url":   "",
+                "name":        "Arcade"
+            },
+            {
+                "icon":        "https://lg-head/portal/morefun/timelapse.png",
+                "name":        "Timelapse"
+            }
+
+        ]
+    }
+}
+
+*/
+
 // Elements are shown in this order
-// name   - used as a key
-// desc   - shown below the image
-// icon   - the image used for an icon, must be in the 'images' directory
-// action - (switch_display|showDoodlesPage)
-// url     - url to switch by the action switch_display
+// name        - shown below the image
+// icon        - the image used for an icon, must be in the 'images' directory
+// display_url - url to switch the display browser
+// kiosk_url   - url to switch the kiosk browser
+
+//TODO: remove two below urls later
 var earthURL = 'http://lg-head/portal-loader.html';
 var timelapseURL = 'file:///mnt/earthtime/data-visualization-tools/examples/webgl-timemachine/landsat.html';
 
-
+/* URL to download the config file from, must be HTTPS */
 var dataURL = "https://lg-head/portal/config.json";
 var data = null;
 
+/* Downloads configuration file from lg-head, it must be done using https,
+   as http requests are blocked by chrome.
+   After downloading the file is parsed, and the section
+   ['portal']['morefun_items'] is copied to the data variable.
+*/
 var downloadConfigData = function(url) {
   $.get( url )
-    .done(function(d){ data = d })
+    .done(function(d){ data = d.portal.morefun_items; initializeMoreFunMenu(); })
     .fail(function(){ console.error("Cannot download config file from: " + dataURL )});
 }
 
-downloadConfigData(dataURL);
 
-var data = [
-  {
-    name: 'earth',
-    desc: 'Maps',
-    icon: 'icon_earth.png',
-    action: 'switch_display',
-    url: earthURL
-  },
-  /*
-  {
-    name: 'timelapse',
-    desc: 'Timelapse',
-    icon: 'timelapse.png',
-    action: 'gotoTimelapse',
-    url: timelapseURL
-  },
-  */
-  {
-    name: 'doodles',
-    desc: 'Arcade',
-    icon: 'doodles.png',
-    action: 'showDoodlesPage'
+/*
+  Changes the display URL.
+  Internally there is a ROS message sent with the URL.
+  The display extension listens to that and redirects the browser.
+*/
+var changeDisplayURL = function(e) {
+  var i = e.target.getAttribute('index');
+  var url = data[i].display_url;
+
+  if (url == null || url == "") {
+    console.log("Cannot change display url to null");
+    return;
   }
-];
 
-// Sends message to background page to change the browser's URL
-var sendSwitchMessage = function(e) {
-  var url = e.target.getAttribute('switch_url');
-  sendSwitchMessageURL(url);
-};
-
-var sendSwitchMessageURL = function(url) {
-  console.log('Trying to switch display to', url);
+  console.log("Changing display url to " + url);
   chrome.runtime.sendMessage({url: url});
-};
+}
 
-var showDoodlesPage = function() {
-  document.location = chrome.extension.getURL('pages/doodles.html');
-};
+/*
+  Changes the kiosk URL - currently this means changing the url
+                          of the browser this extension runs at.
+*/
+var changeKioskURL = function(e) {
+  var i = e.target.getAttribute('index');
+  var url = data[i].kiosk_url;
 
-var goBackToEarthPage = function() {
-  sendSwitchMessageURL(earthURL);
-  document.location = earthURL;
-};
+  if (url == null || url == "") {
+    console.log("Cannot change kiosk url to null");
+    return;
+  }
 
-var gotoTimelapse = function() {
-  sendSwitchMessageURL(timelapseURL);
-};
+  console.log("Changing kiosk url to " + url);
+  document.location = url;
+}
 
+/*
+  Creates the element list for morefun_items element.
+  This is made from the configuration read from lg-head
+*/
 var createElementsList = function() {
 
   var ul = document.createElement('ul');
   for (var i = 0; i < data.length; i++) {
 
-    var name = data[i].name;
-    var desc = data[i].desc;
-    var action = data[i].action;
-    var url = data[i].url;
-    var icon = data[i].icon;
+    var name        = data[i].name;
+    var display_url = data[i].display_url;
+    var kiosk_url   = data[i].kiosk_url;
+    var icon        = data[i].icon;
 
     var li = document.createElement('li');
-    li.setAttribute('switch_url', url);
+    li.setAttribute('index', i);
 
     var img = document.createElement('img');
-    img.setAttribute('src', chrome.extension.getURL('images/' + icon));
-    img.setAttribute('switch_url', url);
+    img.setAttribute('src', icon);
+    img.setAttribute('index', i);
 
     var span = document.createElement('span');
-    span.innerText = desc;
-    span.setAttribute('switch_url', url);
+    span.innerText = name;
+    span.setAttribute('index', i);
 
     li.appendChild(img);
     li.appendChild(span);
 
-    if (action == 'switch_display') {
-      li.onclick = goBackToEarthPage;
-      li.addEventListener(
-        'touchstart',
-        goBackToEarthPage,
-        true
-      );
-    }
-    if (action == 'showDoodlesPage') {
-      li.onclick = showDoodlesPage;
+    li.addEventListener(
+      'touchstart',
+      changeKioskURL,
+      true
+    );
 
-      li.addEventListener(
-        'touchstart',
-        showDoodlesPage,
-        true
-      );
-    }
-    if (action == 'gotoTimelapse') {
-      li.onclick = gotoTimelapse;
-      li.addEventListener(
-        'touchstart',
-        gotoTimelapse,
-        true
-      );
-    }
+    li.addEventListener(
+      'touchstart',
+      changeDisplayURL,
+      true
+    );
 
     ul.appendChild(li);
   }
@@ -126,7 +144,11 @@ var createElementsList = function() {
 
 };
 
-var initializeMoreFun = function() {
+/*
+  Creates all the more fun items according to the configuration
+  from the data variable.
+*/
+var initializeMoreFunMenu = function() {
 
   var onclick = function() {
     document.getElementById('morefun_items').style.visibility = 'visible';
@@ -169,4 +191,4 @@ var initializeMoreFun = function() {
   document.body.addEventListener('touchstart', closeHandler, true);
 };
 
-initializeMoreFun();
+downloadConfigData(dataURL);
