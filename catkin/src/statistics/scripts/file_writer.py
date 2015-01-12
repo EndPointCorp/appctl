9a397dc1531aa8b09f85b5d09c3c30c36c17d445
Wojtek Ziniewicz <wojtek@endpoint.com>
2015-01-12 22:42:31 +0100

    Statistics
    - adjusted interval value
    - added validation of event messages
    - a lot of logger entries for debugging and verbosity
    - finalized file writer
    - some corner case exception handling for service connectivity
    - proper ending of sessions
    - added some TODOs
    - a lot of type/instance asserts for strong type handling
    - keeping current session open at all times

--- catkin/src/statistics/scripts/file_writer.py
+++ catkin/src/statistics/scripts/file_writer.py
@@ -8,6 +8,10 @@ import time
 from statistics.srv import SessionQuery
 
 
+class GeolocationDataException(Exception):
+    pass
+
+
 class FileWriter:
     """
     - periodically collects session events from /statistics/session service
@@ -21,59 +25,88 @@ class FileWriter:
         self.country = rospy.get_param('~country', None)
         self.store_id = rospy.get_param('~store_id', None)
         self.retailer = rospy.get_param('~retailer', None)
-        if self.country == None or\
-           self.store_id == None or\
-           self.retailer == None:
+        if self.country is None or\
+           self.store_id is None or\
+           self.retailer is None:
                raise GeolocationDataException("You need to specify proper store metadata")
         self._wait_for_service()
-        self.json_template = {
-                              "report_time": 0,
-                              "start_ts": 0,
-                              "end_ts": 0,
-                              "metadata":
-                }
         pass
 
     def _wait_for_service(self):
         rospy.loginfo("Waiting for the /statistics/session service to become available")
         rospy.wait_for_service('statistics/session')
         rospy.loginfo("Statistics aggretator service has become available")
+        pass
 
     def _call_service(self):
         service_call = rospy.ServiceProxy('statistics/session', SessionQuery)
         response = service_call(erase=True)
-        rospy.loginfo("Received response from service: %s" % response)
+        rospy.logdebug("Received response from service: %s" % response)
         return response
 
     def _sleep_between_requests(self):
-        rospy.loginfo("Sleeping for %s seconds between /statistics/session call" % self.interval)
-        d = rospy.Duration(self.interval, 0)
-        rospy.sleep(d)
+        rospy.logdebug("Sleeping for %s seconds before writing stats" % self.interval)
+        rospy.sleep(self.interval)
         pass
 
     def _init_node(self):
         rospy.init_node('statistics')
+        pass
 
-    def _compose_json(self, sessions):
-        sessions = sessions.replace("'{", "{").replace("}'", "}")
-        rospy.loginfo("Deserialized sessions => %s" % sessions)
-        for session in json.loads(sessions):
-            rospy.loginfo("Deserialized session => %s" % session)
+    def _jsonize_string(self, string):
+        sessions = string.replace("'{", "{").replace("}'", "}")
+        rospy.logdebug("Deserialized sessions => %s" % sessions)
+        return json.loads(sessions)
 
-        unix_now =  time.time()
+    def _finalize_report_data(self, report_contents):
+        """
+        - fill report_time and end_ts with current time
+        - fill start_ts with current_time minus self.interval
+        """
+        unix_now = int(time.time())
+        report_contents['report_time'] = unix_now
+        report_contents['start_ts'] = unix_now - self.interval
+        report_contents['end_ts'] = unix_now
+        return report_contents
+
+    def _compose_and_write_json(self, sessions):
+        """
+        - jsonize the sessions string received from json aggregator
+        - convert sessions list to json
+        - finalize report and write the file
+        """
+        sessions = self._jsonize_string(sessions)
+        report_contents = {"report_time": 0,
+                           "start_ts": 0,
+                           "end_ts": 0,
+                           "metadata": {"country": self.country,
+                                        "store_id": self.store_id,
+                                        "retailer": self.retailer
+                                        },
+                           "sessions": []
+                           }
+        for session in sessions:
+            report_contents['sessions'].append(session)
+
+        report_contents = self._finalize_report_data(report_contents)
+        with open(self.path + "/" +  str(int(time.time())) + ".json", 'w') as json_file:
+            rospy.loginfo("Writing file %s" % json_file.name)
+            json_file.write(json.dumps(report_contents, sort_keys=True, indent=4, separators=(',', ': ')))
+            del report_contents
 
     def _spin(self):
-        response = self._call_service()
-        json_file = self._compose_json(response.sessions)
-        #json_file.write()
-        #json_file.close()
+        try:
+            response = self._call_service()
+        except rospy.service.ServiceException, e:
+            rospy.logfatal("Could not connect to /statistics/session service because: %s" % e)
+        self._compose_and_write_json(response.sessions)
+        pass
 
     def run(self):
-        rospy.loginfo("Starting session statistics service")
+        rospy.logdebug("Starting session statistics service")
         while not rospy.is_shutdown():
+            self._sleep_between_requests()
             self._spin()
-            rospy.loginfo("Sleeping for %s seconds between /statistics/session call" % self.interval)
-            rospy.sleep(self.interval)
             pass
         pass
 
@@ -81,4 +114,6 @@ if __name__ == '__main__':
     try:
         FileWriter().run()
     except rospy.ROSInterruptException:
+        pass
+    except rospy.service.ServiceException:
         pass
--- catkin/src/statistics/scripts/session_aggregator.py
+++ catkin/src/statistics/scripts/session_aggregator.py
@@ -20,16 +20,17 @@ class SessionAggregator:
     """
     - provides session topic that acts as session events sink
     - provides session service for session event retrieval
+    - since sessions send only "start_ts", we need to end them automatically
+        - populate previous (if exists) session's "end_ts" when new session arrives
+        - if 'erase' flag is received - remove all sessions but last
     """
 
     def __init__(self):
         self.node = self._init_node()
         self.service = self._init_service()
         self.subscriber = self._init_subscriber()
-        self.session_format = { 'start_ts': 0,
-                                'end_ts': 0,
-                                'app_name': 'appname'
-                              }
+        self.event_id = 0
+        self.mandatory_fields = {'start_ts': 0}
         self.sessions = []
         self.max_events = rospy.get_param('~max_events', None)
         self.max_memory = rospy.get_param('~max_memory', '32000000')
@@ -52,8 +53,8 @@ class SessionAggregator:
         """
         Handle the limits and append the event to the sessions storage
         """
-
         self._handle_erase_flag(event)
+        pass
 
         if sys.getsizeof(self.max_memory) > self.max_memory:
             raise OutOfMemoryException
@@ -61,44 +62,86 @@ class SessionAggregator:
         if self.max_events and len(self.sessions) > self.max_events:
             raise TooManyEventsException("Number of events: %s, max: %s" % (len(self.sessions), self.max_events))
 
-        rospy.loginfo("Got session => %s" % event.session)
+        rospy.logdebug("Got session => %s" % event.session)
         if self._validate_incoming_event(event):
-            self.sessions.append(event.session)
+            session = json.loads(event.session)
+            assert isinstance(session, dict)
+            self.sessions.append(session)
+            self._end_previous_session()
         else:
-            rospy.loginfo("Not appending event to session storage")
+            rospy.logdebug("Not appending event to session storage")
+        pass
+
+    def _end_previous_session(self):
+        """
+        If session store has more than 1 session and new session event comes,
+        end previous one using the start time of new one.
+        """
+        if len(self.sessions) >= 2:
+            rospy.logdebug("Ending previous session with time = %s" % self.sessions[-1]['start_ts'])
+            self.sessions[-2]['end_ts'] = self.sessions[-1]['start_ts']
         pass
 
     def _validate_incoming_session(self, event):
         """
-        Checks whether session has keys defined in self.session_format
-        Returns None if any of the required keys are missing
-        Returns `event` if event is valid
+        - Checks whether session has keys defined in self.mandatory_fields
+        - Returns None if any of the required keys are missing
+        - Returns `event` if event is valid
         """
-        session = json.dumps(event.session)
-        for key in self.session_format.iterkeys():
+        session = json.loads(event.session)
+
+        for key in self.mandatory_fields.iterkeys():
             if key not in session:
-                rospy.loginfo("Received event session in wrong format - missing key: %s - please refer to the docs" % str(key))
+                rospy.logwarn("Received event session in wrong format - missing key: %s - please refer to the docs" % str(key))
                 return None
+        # session['serial_number'] = self._get_event_id()
+        # TODO(wz): a incremental serial number for sessions cleared after ROS restart
+        assert isinstance(session, dict)
         return session
 
     def _validate_incoming_event(self, event):
         if event.session:
             return self._validate_incoming_session(event)
         else:
-            rospy.loginfo("Received event session in wrong format - please refer to the docs")
+            rospy.logwarn("Received event session in wrong format - please refer to the docs")
             return None
 
     def _erase_sessions(self):
-        rospy.loginfo("Purging session events.")
-        self.sessions = []
+        """
+        Check whether we have a last started session
+            - if yes - erase all previous sessions and keep current
+            - if not - just do nothing
+        """
+
+        assert isinstance(self.sessions, list)
+
+        if self.sessions:
+            current_session = self.sessions[-1]
+            self.sessions = []
+            assert isinstance(current_session, dict)
+            rospy.logdebug("Purging session events but leaving the current one intact")
+        else:
+            rospy.logdebug("No session is running - keeping the session store intact")
+            current_session = {}
+            self.sessions = []
+
+        rospy.logdebug("Leaving session %s of type %s in session store" % (current_session, type(current_session)))
+        assert isinstance(current_session, dict)
+        if current_session:
+            self.sessions.append(current_session)
         self.erase_flag = 0
+        pass
+
+    def _get_event_id(self):
+        self.event_id += 1
+        return self.event_id
 
     def _handle_erase_flag(self, event):
         if self.erase_flag == 1:
             self._erase_sessions()
         if hasattr(event, 'erase'):
             if event.erase == 1:
-                rospy.loginfo("Received `erase` flag. Session store will be erased upon next request")
+                rospy.logdebug("Received `erase` flag. Session store will be purged upon next request")
                 self.erase_flag = 1
 
     def _process_service_request(self, req):
@@ -107,8 +150,19 @@ class SessionAggregator:
         check the `erase` flag
         """
         self._handle_erase_flag(req)
-        rospy.loginfo("Returning aggregated sessions: %s" % self.sessions)
-        return str(self.sessions)
+        rospy.loginfo("Currently open session: %s" % self.sessions[-1])
+        finished_sessions = json.dumps(self._filter_finished_sessions(self.sessions))
+
+        return finished_sessions
+
+    def _filter_finished_sessions(self, sessions):
+        """
+        Filter out all sessions that don't have an 'end_ts' and return only
+        those that were completed
+        """
+        finished_sessions = [s for s in sessions if 'end_ts' in s.iterkeys()]
+        assert isinstance(finished_sessions, list)
+        return finished_sessions
 
     def _init_service(self):
         service = rospy.Service('statistics/session',
