7a59903500bd766cd51aca28f679c846bc59389b
Matt Vollrath <matt@endpoint.com>
2015-01-16 18:06:58 -0500

    portal_config: read url parameter per request
    This may simplify testing.

--- catkin/src/portal_config/scripts/serve_config.py
+++ catkin/src/portal_config/scripts/serve_config.py
@@ -4,14 +4,20 @@ import rospy
 import urllib2
 from portal_config.srv import *
 
+NODE_NAME = 'portal_config'
+SRV_QUERY = '/'.join(('', NODE_NAME, 'query'))
+PARAM_URL = '~url'
+DEFAULT_URL = 'http://lg-head/portal/config.json'
+
 # XXX TODO: return an error if the config file isn't valid JSON
 
 class ConfigRequestHandler():
-    def __init__(self, url):
-        self.url = url
+    def get_url(self):
+        return rospy.get_param(PARAM_URL, DEFAULT_URL)
 
     def get_config(self):
-        response = urllib2.urlopen(self.url)
+        url = self.get_url()
+        response = urllib2.urlopen(url)
         return response.read()
 
     def handle_request(self, request):
@@ -20,13 +26,15 @@ class ConfigRequestHandler():
 
 
 def main():
-    rospy.init_node('portal_config')
-    url = rospy.get_param('~url', 'http://lg-head/portal/config.json')
+    rospy.init_node(NODE_NAME)
 
-    handler = ConfigRequestHandler(url)
+    #if not rospy.has_param(PARAM_URL):
+    #    rospy.set_param(PARAM_URL, DEFAULT_URL)
+
+    handler = ConfigRequestHandler()
 
     s = rospy.Service(
-        '/portal_config/query',
+        SRV_QUERY,
         PortalConfig,
         handler.handle_request
     )
