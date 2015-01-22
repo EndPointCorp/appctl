e58941ea5788d156cfdb155df4ad302eeac29630
Zdenek Maxa <zdenek@endpoint.com>
2015-01-22 23:14:30 +0100

    onboard node and application configuration, touch: #134
    -onboard conf taken from
    https://github.com/EndPointCorp/lg-live-image/blob/bc18a4640d8a1a5424ff6c25e7fc5ce1a9b76ac7/config/includes.chroot/home/lg/etc/onboard.dconf
    works straight away, yet come paths in the .dconf file are not satisfied
    
    -onboard ROS node made configurable through portal.launch file

--- catkin/src/onboard/scripts/listener.py
+++ catkin/src/onboard/scripts/listener.py
@@ -6,20 +6,28 @@ import rospy
 from std_msgs.msg import Bool
 
 
+NODE_NAME = "onboard_listener"
+CONFIG_FILE = "~config"  # taken from the portal.launch parameter value
+DEFAULT_CONFIG_FILE = os.getenv("HOME") + "/etc/onboard.dconf"
+
+
 def callback(data):
-    # TODO: use python dbus bindings instead of dbus cli
+    config_file = rospy.get_param(CONFIG_FILE, DEFAULT_CONFIG_FILE)
+    rospy.loginfo("%s using '%s' conf file." % (NODE_NAME, config_file))
+    # TODO:
+    #   use python dbus bindings instead of dbus cli
     if data.data:
-        os.system('dconf load /org/onboard/ < ' + os.getenv('HOME') + '/etc/onboard.dconf')
+        os.system('dconf load /org/onboard/ < %s' % config_file)
         os.system('dbus-send --type=method_call --dest=org.onboard.Onboard /org/onboard/Onboard/Keyboard org.onboard.Onboard.Keyboard.Show')
     else:
         os.system('dbus-send --type=method_call --dest=org.onboard.Onboard /org/onboard/Onboard/Keyboard org.onboard.Onboard.Keyboard.Hide')
 
 
 def listener():
-    rospy.init_node('onboard_listener')
+    rospy.init_node(NODE_NAME)
     rospy.Subscriber('/onboard/visibility', Bool, callback)
-
     rospy.spin()
+
 
 if __name__ == '__main__':
     listener()
