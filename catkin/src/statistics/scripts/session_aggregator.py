50676b6c33a692e103253ab3df583efc0710d490
Matt Vollrath <matt@endpoint.com>
2015-03-16 23:29:11 -0400

    Fix session_aggregator import

--- catkin/src/statistics/scripts/session_aggregator.py
+++ catkin/src/statistics/scripts/session_aggregator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 
-import portal_statistics.session_aggregator
+from portal_statistics import session_aggregator
 
 if __name__ == '__main__':
     session_aggregator.main()
