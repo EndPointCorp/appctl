7a2f27e52ec78be67ec74dd16afcb3610e29e83c
Wojciech Ziniewicz <wojciech.ziniewicz@gmail.com>
2015-03-31 12:13:09 +0200

    Moved requirements.txt to tests directory

--- catkin/src/statistics/scripts/session_aggregator.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
-
-from portal_statistics import session_aggregator
-
-if __name__ == '__main__':
-    session_aggregator.main()
