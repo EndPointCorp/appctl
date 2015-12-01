6ccf5e2099b00c32db6becc7c94aff7a042d9f84
Matt Vollrath <matt@endpoint.com>
2015-12-01 01:04:35 -0500

    Better nomenclature for spawn hooks

--- appctl/src/appctl_support/proc_runner.py
+++ appctl/src/appctl_support/proc_runner.py
@@ -84,11 +84,11 @@ class ProcRunner(threading.Thread):
             shell=self.shell,
             close_fds=True
         )
-        self._spawn()
+        self._run_spawn_hooks()
         self.spawn_count += 1
         rospy.loginfo('started process {}'.format(self.proc.pid))
 
-    def _spawn(self):
+    def _run_spawn_hooks(self):
         """
         Calls all _spawn_hooks if any exist
         """
