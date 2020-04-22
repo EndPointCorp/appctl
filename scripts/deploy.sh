a4ef55db447e287b682b08526cd19e203061091c
Will Plaut <will@endpoint.com>
2020-04-22 13:50:27 -0400

    Fix deb deploy path

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,6 +11,6 @@ eval $(ssh-agent -s)
 
 ssh -o StrictHostKeychecking=no aptly@${APTLY_SERVER} mkdir -p incoming/appctl/master/
 cd /catkin_ws
-scp -o StrictHostKeychecking=no *.deb aptly@${APTLY_SERVER}:incoming/appctl/master/
+scp -o StrictHostKeychecking=no ./debs/*.deb aptly@${APTLY_SERVER}:incoming/appctl/master/
 ssh -o StrictHostKeychecking=no  aptly@${APTLY_SERVER} bash /home/aptly/bin/publish-incoming.sh --project appctl --branch master --rosrel "melodic" --distro "bionic"
 ssh -o StrictHostKeychecking=no aptly@${APTLY_SERVER} bash /home/aptly/bin/publish-incoming-seperate-repos.sh --project appctl --branch master --rosrel "melodic" --distro "bionic"
