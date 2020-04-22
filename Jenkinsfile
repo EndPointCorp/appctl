b9aa703116162833951d72a34f415bb245dffb22
Will Plaut <will@endpoint.com>
2020-04-22 13:24:07 -0400

    Fix deployment

--- Jenkinsfile
+++ Jenkinsfile
@@ -24,7 +24,7 @@ pipeline {
     }
     stage('Deploy') {
       when {
-        branch 'mater'
+        branch 'master'
       }
       steps {
         sh "./scripts/deploy.sh"
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,6 +11,6 @@ eval $(ssh-agent -s)
 
 ssh aptly@ln105.endpoint.com mkdir -p incoming/appctl/master/
 cd /catkin_ws
-scp *.dev aptly@${APTLY_SERVER}:incoming/appctl/master/
+scp *.deb aptly@${APTLY_SERVER}:incoming/appctl/master/
 ssh aptly@${APTLY_SERVER} bash /home/aptly/bin/publish-incoming.sh --project appctl --branch master --rosrel "melodic" --distro "bionic"
 ssh aptly@${APTLY_SERVER} bash /home/aptly/bin/publish-incoming-seperate-repos.sh --project appctl --branch master --rosrel "melodic" --distro "bionic"
