27c342122a0feae0943ec16dc20f036a46975955
Will Plaut <will@endpoint.com>
2020-04-23 10:40:15 -0400

    Fix branch in deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,8 +9,8 @@ chown -R root:root /root/.ssh
 chmod 0600 /root/.ssh/id_rsa
 eval $(ssh-agent -s)
 
-ssh -o StrictHostKeychecking=no aptly@${APTLY_SERVER} mkdir -p incoming/appctl/master/
+ssh -o StrictHostKeychecking=no aptly@${APTLY_SERVER} mkdir -p incoming/appctl/origin/master/
 cd /catkin_ws
-scp -o StrictHostKeychecking=no ./debs/*.deb aptly@${APTLY_SERVER}:incoming/appctl/master/
+scp -o StrictHostKeychecking=no ./debs/*.deb aptly@${APTLY_SERVER}:incoming/appctl/origin/master/
 ssh -o StrictHostKeychecking=no  aptly@${APTLY_SERVER} bash /home/aptly/bin/publish-incoming.sh --project appctl --branch origin/master --rosrel "melodic" --distro "bionic"
 ssh -o StrictHostKeychecking=no aptly@${APTLY_SERVER} bash /home/aptly/bin/publish-incoming-separate-repos.sh --project appctl --branch origin/master --rosrel "melodic" --distro "bionic"
